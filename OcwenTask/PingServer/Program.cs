e8f7e427351e291459b6f6b5fc5a63ea288734b8
Rahamad89 <jrahamadulla@gmail.com>
2016-11-23 23:14:14 +0530

    Rahamad
    check in

---
+++ OcwenTask/PingServer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Ping p1 = new Ping();
+                PingReply PR = p1.Send("199.21.60.52:7990");
+                Console.WriteLine("Server Response******" + PR.Status.ToString());
+                Console.WriteLine("Round Trip Time******" + PR.RoundtripTime.ToString());
+               
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                
+            }
+               Console.ReadLine();
+        }
+        
+    }
+}
---
+++ OcwenTask/WebResponseChecker/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebResponseChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyWebRequest myWebRequest = new MyWebRequest("http://199.21.60.52:7990", "GET");
+            List<string> webResponse = new List<string>();
+            webResponse = myWebRequest.GetResponse();
+            Console.WriteLine("Web Response:"+webResponse[0]);
+            if (webResponse[1] == "OK")
+                Console.WriteLine("*****************Response:SUCCESS************************");
+            else
+                Console.WriteLine("*****************Response:FAILURE************************");
+            Console.ReadKey();
+            
+        }
+    }
+}
